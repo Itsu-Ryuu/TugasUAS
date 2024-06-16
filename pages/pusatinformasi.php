1b0c21e45cf7a470e9dc541d128f6e97cc82960e
Itsu-Ryuu <agustiheryunanda@gmail.com>
2024-06-16 22:56:32 +0700

    Tambahan

--- pages/pusatinformasi.php
+++ pages/pusatinformasi.php
@@ -16,5 +16,12 @@
 
             <p>Menjadi Perusahaan Listrik Terkemuka se-Asia Tenggara dan #1 Pilihan Pelanggan untuk Solusi Energi.</p>
         </main>
+        <main class="container mt-3">
+            <h2>Misi</h2>
+            
+            <p>Menjalankan bisnis kelistrikan dan bidang lain yang terkait, berorientasi pada kepuasan pelanggan, anggota perusahaan dan pemegang saham.</p>
+            Menjadikan tenaga listrik sebagai media untuk meningkatkan kualitas kehidupan masyarakat.
+            Mengupayakan agar tenaga listrik menjadi pendorong kegiatan ekonomi.
+            Menjalankan kegiatan usaha yang berwawasan lingkungan.
     </div>
 <?php include "./../layout/footer.php" ?>
