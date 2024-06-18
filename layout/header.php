20faa94c19d828d59c8b286942f11b1294c62ca4
afriyanpm15 <afriyanpm15@gmail.com>
2024-06-18 19:33:44 +0700

    push logo

--- layout/header.php
+++ layout/header.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>ListrikRumahTangga</title>
+    <link rel="icon" type="image/jpeg" href="./../images/bg-PLN.jpg">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="./../css/style.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
@@ -11,7 +12,7 @@
 <body>
     <nav class="navbar navbar-expand-lg custom-navbar d-flex">
         <div class="container-fluid">
-          <a class="navbar-brand" href="./../pages/index.php">PLN JAKARTA</a>
+          <a class="navbar-brand" href="./../pages/index.php"><img src="./../images/bg-PLN.jpg" alt=".." class="w-10">PLN JAKARTA</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
