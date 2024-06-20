5ab22d440f7447fb7632df3ed68f0747a584864d
Itsu-Ryuu <agustiheryunanda@gmail.com>
2024-06-20 17:53:57 +0700

    perubahan 20 juni

--- pages/profilpengguna.php
+++ pages/profilpengguna.php
@@ -3,14 +3,21 @@
     include "./../layout/header.php";
     include "./../service/profilePengguna.php";
 ?>
+
+    <link href="./../css/style.css">
+
+    <div class="profile-container">
+            <img src="./../images/Default.png"> 
+    </div>
     <div class="content-wrapper">
         <main class="container mt-5 text-center">
-            <h1>Selamat Datang <?= $_SESSION["Nama"] ?> Pengguna Layanan PLN</h1>
+            <h3>Selamat Datang <?= $_SESSION["Nama"] ?> Pengguna Layanan PLN</h3>
         </main>
     <div class="content-wrapper">
-        <main class="container mt-5 text-bottom">
+        <main class="container mt-5 text-center">
                 <h3>Terima Kasih sudah memakai layanan PLN</h3>
         </main>
     </div>
-
+    <script src="script.js"></script>
+    
 <?php include "./../layout/footer.php" ?>
