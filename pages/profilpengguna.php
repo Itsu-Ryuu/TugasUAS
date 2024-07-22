74037f4bac01daeb384ca9fa3f405a256b8132c6
afriyanpm15 <afriyanpm15@gmail.com>
2024-07-22 18:13:50 +0700

    push

--- pages/profilpengguna.php
+++ pages/profilpengguna.php
@@ -11,7 +11,7 @@ $nomer = 1;
             <div class="profile-container">
                 <img src="./../images/Default.png">
             </div>
-            
+
         </div>
         <div class="col-md-8 d-flex justify-content-center align-items-center">
             <main class="container text-center">
@@ -19,40 +19,41 @@ $nomer = 1;
             </main>
         </div>
     </div>
-<div class="d-flex justify-content-center align-items-center mt-4">
-    <div class="row ">
-        <div class="col-md-12 text-center" style="padding-top: 2px;">
-            <table class="table table-dark table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">No</th>
-                        <th scope="col">Nama</th>
-                        <th scope="col">Id Pelanggan</th>
-                        <th scope="col">Alamat</th>
-                        <th scope="col">Nomor Meteran</th>
-                        <th scope="col">Golongan</th>
-                    </tr>
-                </thead>
-                <tbody>
-                        <tr>
-                            <th scope="row"><?= $nomer++ ?></th>
-                            <td><?= $_SESSION['Nama'] ?></td>
-                            <td><?= $data['id_pelanggan'] ?></td>
-                            <td><?= $data['Alamat'] ?></td>
-                            <td><?= $data['nomor_meteran'] ?></td>
-                            <td><?= $data['golongan'] ?></td>
-                        </tr>
-                </tbody>
-            </table>
-        </div>
+    <div class="d-flex justify-content-center align-items-center mt-4">
+        <div class="row ">
+            <?php if (!isset($_SESSION['is_admin'])) : ?>
+                <div class="col-md-12 text-center" style="padding-top: 2px;">
+                    <table class="table table-dark table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">No</th>
+                                <th scope="col">Nama</th>
+                                <th scope="col">Id Pelanggan</th>
+                                <th scope="col">Alamat</th>
+                                <th scope="col">Nomor Meteran</th>
+                                <th scope="col">Golongan</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row"><?= $nomer++ ?></th>
+                                <td><?= $_SESSION['Nama'] ?></td>
+                                <td><?= $data['id_pelanggan'] ?></td>
+                                <td><?= $data['Alamat'] ?></td>
+                                <td><?= $data['nomor_meteran'] ?></td>
+                                <td><?= $data['golongan'] ?></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            <?php endif; ?>
+            <div class="content-wrapper d-flex flex-column" style="min-height: 20vh;">
+                <div class="mt-auto">
+                    <main class="container text-center">
+                        <h3>Terima Kasih sudah memakai layanan PLN</h3>
+                    </main>
+                </div>
+            </div>
+            <script src="script.js"></script>
 
-    <div class="content-wrapper d-flex flex-column" style="min-height: 20vh;">
-    <div class="mt-auto">
-        <main class="container text-center">
-            <h3>Terima Kasih sudah memakai layanan PLN</h3>
-        </main>
-    </div>
-</div>
-<script src="script.js"></script>
-
-<?php include "./../layout/footer.php" ?>
+            <?php include "./../layout/footer.php" ?>
